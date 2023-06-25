8070685c891504ff6b246772976243cbc9276c89
Victor Henrique <101911985+VictorLeda1@users.noreply.github.com>
2023-06-25 15:56:58 -0300

    Manutenção na mensagem de erro do Login

--- login.php
+++ login.php
@@ -1,46 +1,6 @@
 <?php
 
 include('./includes/conexao.php');
-
-/*    */
-if (isset($_POST['usuario']) || isset($_POST['senha'])) {
-  if (strlen($_POST['usuario']) == 0) {
-    echo 'Usuário não preenchido';
-  } elseif (strlen($_POST['senha']) == 0) {
-    echo 'Senha não preenchida';
-  } else {
-
-    /*    */
-    $usuario = $mysqli->real_escape_string($_POST['usuario']);
-    $senha = $mysqli->real_escape_string($_POST['senha']);
-
-    /*    */
-    $sql_code = "SELECT * FROM login WHERE username = '$usuario'";
-    $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
-    $quantidade = $sql_query->num_rows;
-
-    /*    */
-    if ($quantidade == 1) {
-      $conta = $sql_query->fetch_assoc();
-      if (password_verify($senha, $conta['senha'])) {
-        if (!isset($_SESSION)) {
-          session_start();
-        }
-        $_SESSION['id'] = $conta['id'];
-        $_SESSION['usuario'] = $conta['username'];
-
-        if ($_SESSION['id'] == 1) {
-          header('Location: cozinha.php');
-        } else {
-          header('Location: cadastroPedido.php');
-        }
-      } else {
-        echo "Falha ao logar! E-mail ou senha incorretos";
-      }
-    }
-  }
-}
-
 include('./includes/head.php');
 
 ?>
@@ -60,6 +20,51 @@ include('./includes/head.php');
         </div>
         <button type="submit">Login</button>
       </form>
+      <br>
+      <?php
+      if (isset($_POST['usuario']) || isset($_POST['senha'])) {
+        if (strlen($_POST['usuario']) == 0) {
+          echo 'Usuário não preenchido';
+        } elseif (strlen($_POST['senha']) == 0) {
+          echo 'Senha não preenchida';
+        } else {
+
+          /*    */
+          $usuario = $mysqli->real_escape_string($_POST['usuario']);
+          $senha = $mysqli->real_escape_string($_POST['senha']);
+
+          /*    */
+
+          if ($sql_code = "SELECT * FROM login WHERE username = '$usuario'") {
+
+            $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+            $quantidade = $sql_query->num_rows;
+
+            /*    */
+            if ($quantidade == 1) {
+              $conta = $sql_query->fetch_assoc();
+              if (password_verify($senha, $conta['senha'])) {
+                if (!isset($_SESSION)) {
+                  session_start();
+                }
+                $_SESSION['id'] = $conta['id'];
+                $_SESSION['usuario'] = $conta['username'];
+
+                if ($_SESSION['id'] == 1) {
+                  header('Location: cozinha.php');
+                } else {
+                  header('Location: cadastroPedido.php');
+                }
+              } else {
+                echo "Falha ao logar! Senha incorreta";
+              }
+            } else {
+              echo "Falha ao logar! Usuario ou Senha incorreta";
+            }
+          }
+        }
+      }
+      ?>
     </div>
   </div>
 </body>
